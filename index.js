6184f5c8449957db987ee2a5836479400640727e
Gavin Veitch <gavintveitch@gmail.com>
2022-09-30 09:13:11 -0600

    commit tree 5fa11770a51fcd42a06db6fef58a7feee05f112a

--- index.js
+++ index.js
@@ -12,5 +12,18 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((string1) => {
+    const brokenArray = [] = string1.split(" ")
+
+    const brokenArray2 = [] = brokenArray.map((string2) => string2.charAt(0).toUpperCase() + string2.slice(1))
+
+    const fixedArray  = /*[] = brokenArray2.map(() =>*/ brokenArray2.join(" ")//this dupes it for some reason
+    const fixedArrayLength = fixedArray.length
+
+    /*const correctArray = []
+    for(let counter = 0; counter < fixedArrayLength; counter ++) {
+       correctArray.push(fixedArray[counter])
+    }*/
+    return fixedArray
+  })
 }
